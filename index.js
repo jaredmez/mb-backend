2495679b01d46041168d334cb0200762bd6116d3
Jared M <mezquitaj@gmail.com>
2021-10-28 07:44:59 -0400

    prep for deployment

--- index.js
+++ index.js
@@ -5,25 +5,10 @@ const Song = require('./models/song')
 const cors = require('cors')
 
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3002
 
 app.use(cors())
 app.use(express.json())
-
-songs = [
-    {
-    artist: "Drake",
-    artistId: "b49b81cc-d5b7-4bdd-aadb-385df8de69a6",
-    name: "Hold On, We're Going Home",
-    playcount: "4503634"
-    },
-    {
-    artist: "Eminem",
-    artistId: "f207eac0-829f-41e5-9dc9-d7c3263de3ed",
-    name: "Lose Yourself",
-    playcount: "6098613"
-    }
-]
 
 app.get('/api/songs', (req, res) => {
     Song.find({}).then(result => {
